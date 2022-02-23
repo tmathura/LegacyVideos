f4a65246408786aacb086c94a4088e14f0dedf82
tmathura <tmathura@gmail.com>
2022-02-23 19:13:12 +0200

    Add Stored Procedures/movies_ins.sql. Remove announced_date on movies table.

---
+++ Database/Migrations/006_20220223-1911_teshvierm_alter_table_movies_delete_column_announced_date.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="fd925be1-76e8-4f4a-aa06-99c15307af38" />
+GO
+
+PRINT N'Altering [dbo].[movies]'
+GO
+IF COL_LENGTH(N'[dbo].[movies]', N'announced_date') IS NOT NULL
+ALTER TABLE [dbo].[movies] DROP COLUMN [announced_date]
+GO
---
+++ Database/Programmable Objects/dbo/Stored Procedures/movies_ins.sql
@@ -0,0 +1,45 @@
+IF OBJECT_ID('[dbo].[movies_ins]') IS NOT NULL
+	DROP PROCEDURE [dbo].[movies_ins];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE PROCEDURE [dbo].[movies_ins]
+(
+    @title [VARCHAR](200),
+    @description [VARCHAR](2000),
+    @movie_type_id [INT],
+    @duration [INT],
+    @release_date [DATETIME],
+    @added_date [DATETIME],
+    @owned [BIT]
+)
+AS
+BEGIN
+
+    INSERT INTO dbo.movies
+    (
+        title,
+        [description],
+        movie_type_id,
+        duration,
+        release_date,
+        added_date,
+        owned
+    )
+    SELECT @title,         -- title - varchar(200)
+           @description,   -- description - varchar(2000)
+           @movie_type_id, -- movie_type_id - int
+           @duration,      -- duration - int
+           @release_date,  -- release_date - datetime
+           @added_date,    -- added_date - datetime
+           @owned          -- owned - bit
+    ;
+
+    RETURN SCOPE_IDENTITY();
+    
+END;
+GO

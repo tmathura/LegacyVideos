878e5e30fe26edb761adf89e2c0b33484334e236
tmathura <tmathura@gmail.com>
2022-02-23 10:20:18 +0200

    Add tables movies and rename scripts

---
+++ Database/Manual Scripts/202202221327_create_table_movie.sql
@@ -0,0 +1,37 @@
+IF NOT EXISTS
+(
+    SELECT *
+    FROM INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_SCHEMA = 'dbo'
+          AND TABLE_NAME = 'movies'
+)
+BEGIN
+    CREATE TABLE [dbo].[movies]
+    (
+        [id] [INT] IDENTITY(1, 1) NOT NULL,
+        [title] [VARCHAR](200) NOT NULL,
+        [description] [VARCHAR](2000) NOT NULL,
+        [movie_type_id] [INT] NOT NULL,
+        [duration] [INT] NOT NULL,
+        [release_date] [DATETIME] NOT NULL,
+        [announced_date] [DATETIME] NOT NULL,
+        [added_date] [DATETIME] NOT NULL,
+        [owned] [BIT] NOT NULL CONSTRAINT DF_movies_owned DEFAULT 0,
+        CONSTRAINT [PK_movies_id] PRIMARY KEY CLUSTERED ([id] ASC) ON [PRIMARY]
+    ) ON [PRIMARY];
+END;
+GO
+
+IF NOT EXISTS
+(
+    SELECT *
+    FROM sys.foreign_keys
+    WHERE object_id = OBJECT_ID(N'[dbo].[FK_movies_movie_type_id]')
+          AND parent_object_id = OBJECT_ID(N'[dbo].[movies]')
+)
+BEGIN
+    ALTER TABLE dbo.[movies] WITH CHECK
+    ADD CONSTRAINT [FK_movies_movie_type_id]
+        FOREIGN KEY ([movie_type_id])
+        REFERENCES dbo.[movie_type] ([id]);
+END;
---
+++ Database/Migrations/002_20220223-1017_teshvierm_add_table_movies.sql
@@ -0,0 +1,29 @@
+﻿-- <Migration ID="aa16b2b4-ba53-4fb7-bff6-07383e9354c4" />
+GO
+
+PRINT N'Creating [dbo].[movies]'
+GO
+IF OBJECT_ID(N'[dbo].[movies]', 'U') IS NULL
+CREATE TABLE [dbo].[movies]
+(
+[id] [int] NOT NULL IDENTITY(1, 1),
+[title] [varchar] (200) NOT NULL,
+[description] [varchar] (2000) NOT NULL,
+[movie_type_id] [int] NOT NULL,
+[duration] [int] NOT NULL,
+[release_date] [datetime] NOT NULL,
+[announced_date] [datetime] NOT NULL,
+[added_date] [datetime] NOT NULL,
+[owned] [bit] NOT NULL CONSTRAINT [DF_movies_owned] DEFAULT ((0))
+)
+GO
+PRINT N'Creating primary key [PK_movies_id] on [dbo].[movies]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PK_movies_id]', 'PK') AND parent_object_id = OBJECT_ID(N'[dbo].[movies]', 'U'))
+ALTER TABLE [dbo].[movies] ADD CONSTRAINT [PK_movies_id] PRIMARY KEY CLUSTERED ([id])
+GO
+PRINT N'Adding foreign keys to [dbo].[movies]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_movies_movie_type_id]','F') AND parent_object_id = OBJECT_ID(N'[dbo].[movies]', 'U'))
+ALTER TABLE [dbo].[movies] ADD CONSTRAINT [FK_movies_movie_type_id] FOREIGN KEY ([movie_type_id]) REFERENCES [dbo].[movie_type] ([id])
+GO

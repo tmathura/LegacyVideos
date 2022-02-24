2f6232cfc005a6aba15d931f2f7d67592af0c638
tmathura <tmathura@gmail.com>
2022-02-24 22:13:16 +0200

    Add stored procedure series_upd and unit test

---
+++ Database/Programmable Objects/dbo/Stored Procedures/series_upd.sql
@@ -0,0 +1,35 @@
+IF OBJECT_ID('[dbo].[series_upd]') IS NOT NULL
+	DROP PROCEDURE [dbo].[series_upd];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE PROCEDURE [dbo].[series_upd]
+(
+    @id INT,
+    @title [VARCHAR](200),
+    @description [VARCHAR](2000),
+    @release_date [DATETIME],
+    @added_date [DATETIME],
+    @ended [BIT],
+    @owned [BIT],
+    @anime [BIT]
+)
+AS
+BEGIN
+
+    UPDATE dbo.series
+    SET title = @title,               -- title - varchar(200)
+        [description] = @description, -- description - varchar(2000)
+        release_date = @release_date, -- release_date - datetime
+        added_date = @added_date,     -- added_date - datetime
+        ended = @ended,               -- ended - bit
+        owned = @owned,               -- owned - bit
+        anime = @anime                -- anime - bit
+    WHERE id = @id;
+
+END;
+GO

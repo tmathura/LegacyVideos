5dcc6428462a7dd2bd6ad48b5c72918b2cdc6df8
tmathura <tmathura@gmail.com>
2022-03-01 19:38:11 +0200

    Add test AddMovie_Invalid_MovieType

--- LegacyVideos.WebApi.IntegrationTests/Controllers/MoviesControllerTests.cs
+++ LegacyVideos.WebApi.IntegrationTests/Controllers/MoviesControllerTests.cs
@@ -1,7 +1,7 @@
-using System;
 using LegacyVideos.WebApi.IntegrationTests.Common.Helpers;
 using Newtonsoft.Json;
 using RestSharp;
+using System;
 using System.Net;
 using System.Threading.Tasks;
 using Xunit;
@@ -67,6 +67,39 @@ namespace LegacyVideos.WebApi.IntegrationTests.Controllers
             Assert.Equal(Convert.ToDateTime(releaseDate), Convert.ToDateTime(dbRow[0]["release_date"]));
             Assert.Equal(Convert.ToDateTime(addedDate), Convert.ToDateTime(dbRow[0]["added_date"]));
             Assert.Equal(owned, Convert.ToBoolean(dbRow[0]["owned"]));
+        }
+
+        /// <summary>
+        /// Add a new move and validate that the response to an internal server error.
+        /// </summary>
+        [Fact]
+        public async Task AddMovie_Invalid_MovieType()
+        {
+            // Arrange
+            const string title = "The Matrix";
+            const string description = "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.";
+            const int movieType = 3;
+            const int duration = 136;
+            const string releaseDate = "1999-03-30";
+            const string addedDate = "2022-03-01";
+            const bool owned = true;
+
+            var movieRequest = new
+            {
+                title = title,
+                description = description,
+                movietype = movieType,
+                duration = duration,
+                releasedate = releaseDate,
+                addeddate = addedDate,
+                owned = owned
+            };
+
+            // Act
+            var response = await _commonHelper.CallEndPoint("api/movies", Method.Post, movieRequest);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
         }
     }
 }
